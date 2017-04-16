5ff419183b4d5abca94bac0fb045764fc49ebfdc
meganhejja <megan.upp@gmail.com>
2017-04-16 12:01:01 -0700

    Failing test before fixing intermediate step

--- packageLoader.java
+++ packageLoader.java
@@ -3,7 +3,10 @@ package packageLoader;
 public class packageLoader {
     
     public static String createOrderedList(String[] inputs) {
-        return inputs[0].replace(":","");
+        String firstEntry  = inputs[0].replace(":", "").trim();
+        String secondEntry = inputs[1].replace(":", "").trim();
+        
+        return firstEntry + ", " + secondEntry;
     }
        
 }
--- packageLoaderTest.java
+++ packageLoaderTest.java
@@ -6,8 +6,20 @@ import static org.junit.Assert.assertEquals;
 public class packageLoaderTest{
       
     @Test
-    public void whenGivenAnArrayOfOneValueItShouldReturnThatValue() {
+    public void whenGivenAnArrayOfOneValueNoDependenciesItShouldReturnThatValue() {
         String [] testInput = new String [] {"Megan Package:"};
         assertEquals("Megan Package", packageLoader.createOrderedList(testInput));
+    }
+    
+    @Test
+    public void whenGivenAnArrayOfOneValueWithDependenciesItShouldReturnValueCommaDependency() {
+        String [] testInput = new String [] {"KittenService: CamelCaser"};
+        assertEquals("KittenService, CamelCaser", packageLoader.createOrderedList(testInput));
+    }
+    
+    @Test
+    public void whenGivenAnArrayOfTwoValuesWithNoDependenciesItShouldReturnValueCommaValue() {
+        String [] testInput = new String [] {"KittenService: ", "CamelCaser: "};
+        assertEquals("KittenService, CamelCaser", packageLoader.createOrderedList(testInput).toString());
     }
 }
